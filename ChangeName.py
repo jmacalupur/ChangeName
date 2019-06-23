a6911a293d594d9fc60999371742d8eac29b33d5
jmacalupur <macalupu.j@gmail.com>
2019-06-23 14:52:03 -0500

    Version Inicial

---
+++ ChangeName.py
@@ -0,0 +1,39 @@
+import os
+import time
+import progressbar
+
+list_name = os.listdir()
+list_name_lenght = len(list_name)
+new_list = []
+new_list_updated = []
+
+def _create_list():
+    global list_name_lenght, list_name, new_list
+    print('Creando listas...')
+    for i in progressbar.progressbar(range(list_name_lenght)):
+        if old_name in list_name[i]:
+            new_list.append(list_name[i])
+        time.sleep(0.02)
+
+def _create_list_updated():
+    print('Creando nueva lista de cambio de nombre...')
+    global new_list, new_list_updated
+    new_list_updated = [name.replace(old_name, new_name) for name in new_list]
+
+
+def _change_filename():
+    print('Cambiando los nombres de los archivos...')
+    global new_list, new_list_updated
+    for i in progressbar.progressbar(range(len(new_list))):
+        os.rename(new_list[i], new_list_updated[i])
+        time.sleep(0.02)
+
+
+if __name__ == '__main__':
+    old_name = input('Coloca la fecha antigua del archivo: ')
+    new_name = input('Coloca la fecha nueva del archivo: ')
+    _create_list()
+    _create_list_updated()
+    _change_filename()
+    print('**PROCESO TERMINADO**')
+
